ab92c99e99a9a2d87b13eff2e59d869658c297be
Gary Evans <gary.evans@hpe.com>
2015-08-31 21:43:35 +0000

    Subject: Story 2439 - ops-utils repo migration.
    This is first step.
    1) reorg'ed the files in the repo
    2) ADD ops-* versions of the hc-* files
    
    ...after all repos that depend on hc-* have been modified to use the
    ops-* versions, then I will remove the hc-* versions from this repo.
    
    Change-Id: I130137c04c42f63b87474ff1ad5f093534cc4042
    Signed-off-by: Gary Evans <gary.evans@hpe.com>

---
+++ include/ops-utils.h
@@ -0,0 +1,149 @@
+/*
+ Copyright (C) 2015 Hewlett-Packard Development Company, L.P.
+ All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may
+    not use this file except in compliance with the License. You may obtain
+    a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+    License for the specific language governing permissions and limitations
+    under the License.
+*/
+
+/************************************************************************//**
+ * @defgroup ops_utils Core Utilities
+ * This library provides common utility functions used by various OpenSwitch
+ * processes.
+ * @{
+ *
+ * @defgroup ops_utils_public Public Interface
+ * Public API for ops_utils library.
+ *
+ * - Math: A set of functions that do conversion from one format to another.
+ * - PID: A set of functions used to record and read dameon process id.
+ *
+ * @{
+ *
+ * @file
+ * Header for ops_utils library.
+ ***************************************************************************/
+
+#ifndef __OPS_UTILS_H_
+#define __OPS_UTILS_H_
+
+#include <netinet/ether.h>
+
+/******************* MATH *************************/
+
+#define OPS_MAC_STR_SIZE	18    /*!< Number of bytes in a MAC ADDR string */
+#define OPS_WWN_STR_SIZE	24    /*!< Number of bytes in a WWN string */
+
+
+/************************************************************************//**
+ * Converts an Ethernet address pointed to by addr to a string
+ * mac_a with leading zeros.
+ *
+ * @param[out] mac_a :MAC address string
+ * @param[in]  addr  :Ethernet address
+ *
+ * @return MAC address string
+ ***************************************************************************/
+extern char *ops_ether_ntoa(char *mac_a, const struct ether_addr *addr);
+
+/************************************************************************//**
+ * This function is a World Wide Name version of ops_ether_ntoa and converts
+ * a WWN address pointed to by wwn to a string wwn_a with leading zeros.
+ *
+ * @param[out] wwn_a :WWN address string
+ * @param[in]  wwn   :World Wide Name
+ *
+ * @return WWN address string
+ ***************************************************************************/
+extern char *ops_wwn_ntoa(char *wwn_a, const char *wwn);
+
+/************************************************************************//**
+ * Converts a generic array of binary octets into
+ * unsigned long long. This can be handy for incrementing MACs or WWNs.
+ *
+ * @param[in]  char_array :input array with most significant byte first
+ * @param[in]  length     :length of input string
+ *
+ * @return ull binary value of input string
+ ***************************************************************************/
+extern unsigned long long
+    ops_char_array_to_ulong_long(unsigned char *char_array, unsigned int length);
+
+/************************************************************************//**
+ * Converts an unsigned long long into generic array of
+ * binary octets. Output is array with most significant byte first
+ *
+ * @param[in]  value      :ull binary value
+ * @param[in]  length     :number of bytes to process
+ * @param[out] char_array :char array with converted bytes
+ *
+ * @return void
+ ***************************************************************************/
+extern void ops_ulong_long_to_char_array(unsigned long long value,
+        unsigned int length, unsigned char *char_array);
+
+/************************************************************************//**
+ * Converts an Ethernet address stored as a 6 byte binary array
+ * into a printable mac address with leading zeros.
+ *
+ * @param[out] mac_a    :MAC address string
+ * @param[in]  addr     :6 byte binary array with MAC address
+ *
+ * @return MAC address string
+ ***************************************************************************/
+extern char *ops_ether_array_to_string(char *mac_a, const unsigned char *addr);
+
+/************************************************************************//**
+ * Converts an Ethernet address stored as a long long
+ * into a printable mac address with leading zeros. returns mac_a.
+ *
+ * @param[out] mac_a  :MAC address string
+ * @param[in]  mac    :ull MAC address
+ *
+ * @return MAC address string
+ ***************************************************************************/
+extern char *ops_ether_ulong_long_to_string(char *mac_a,
+        const unsigned long long mac);
+
+
+/******************* PID Utility *******************/
+
+/************************************************************************//**
+ * Writes the process id to a file.
+ *
+ * @param[in]  filename  :Filename to write pid
+ *
+ * @return zero for success, else errno on failure
+ ***************************************************************************/
+extern int ops_record_pid(const char *filename);
+
+/************************************************************************//**
+ * Reads a process id stored in a file.
+ *
+ * @param[in]  filename  :Filename with the pid
+ *
+ * @return PID if sucessful, else errno on failure
+ ***************************************************************************/
+extern int ops_read_pid(const char *filename);
+
+/************************************************************************//**
+ * Reads the PID from a file based on process name.
+ *
+ * @param[in]  procname  :Process name to use to locate the PID file
+ *
+ * @return PID if sucessful, else errno on failure
+ ***************************************************************************/
+extern int ops_read_pid_by_procname(const char *procname);
+
+#endif /* __OPS_UTILS_H_ */
+/** @} end of group ops_utils_public */
+/** @} end of group ops_utils */
---
+++ src/ops-utils.c
@@ -0,0 +1,201 @@
+/*
+ Copyright (C) 2015 Hewlett-Packard Development Company, L.P.
+ All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may
+    not use this file except in compliance with the License. You may obtain
+    a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+    License for the specific language governing permissions and limitations
+    under the License.
+*/
+
+/*************************************************************************//**
+ * @ingroup ops_utils
+ * This module contains the DEFINES and functions that comprise the ops-utils
+ * library.
+ *
+ * @file
+ * Source file for ops-utils library.
+ *
+ ****************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sched.h>
+#include <string.h>
+#include <errno.h>
+
+#include "ops-utils.h"
+
+/*********************************************************
+ *                      PID Utility                      *
+ *********************************************************/
+
+int
+ops_record_pid(const char *filename)
+{
+	FILE *pid_file;
+
+	pid_file = fopen(filename, "w");
+	if ((FILE *)NULL == pid_file) {
+		return errno;
+	}
+
+	fprintf(pid_file, "%d\n", getpid());
+	fclose(pid_file);
+
+	return 0;
+
+} /* ops_record_pid */
+
+int
+ops_read_pid(const char *filename)
+{
+	FILE *pid_file;
+	int pid;
+
+	pid_file = fopen(filename, "r");
+	if ((FILE *)NULL == pid_file) {
+		return -errno;
+	}
+
+	if (1 != fscanf(pid_file,"%d", &pid)) {
+		pid = -EINVAL;
+	}
+	fclose(pid_file);
+
+	return pid;
+
+} /* ops_read_pid */
+
+int
+ops_read_pid_by_procname(const char *procname)
+{
+	char filename[80];
+
+	snprintf(filename,sizeof(filename),"/var/run/%s.pid", procname);
+
+	return ops_read_pid(filename);
+
+} /* ops_read_pid_by_procname */
+
+
+/*********************************************************
+ *                       MATH                            *
+ *********************************************************/
+
+/*
+ * ops_char_array_to_ulong_long
+ *
+ * Converts a generic array of binary values into unsigned long long.
+ * This can be handy for incrementing MACs or WWNs.
+ * Input is array with most significant byte first
+ */
+unsigned long long
+ops_char_array_to_ulong_long(unsigned char *char_array, unsigned int length)
+{
+	unsigned long long   value = (unsigned long long)0;
+	unsigned int    i;
+
+	for ( i = 0; i < length; i++ ) {
+		value = ( value << 8 ) + char_array[i];
+	}
+
+	return value;
+} /* ops_char_array_to_ulong_long */
+
+/*
+ * ops_ulong_long_to_char_array
+ *
+ * Converts an unsigned long long into generic array of binary values.
+ * Output is array with most significant byte first
+ */
+void
+ops_ulong_long_to_char_array(unsigned long long value, unsigned int length,
+		unsigned char *char_array)
+{
+	unsigned long long   temp = value;
+	int     i;
+
+	for ( i = length - 1; i >= 0; i-- ) {
+		char_array[i] = temp & 0xff;
+		temp >>= 8;
+	}
+} /* ops_ulong_long_to_char_array */
+
+/*
+ * ops_ether_ntoa - the pretty version of ether_ntoa
+ *
+ * converts an Ethernet address pointed to by addr to a string mac_a
+ * with leading zeros. returns mac_a.
+ *
+ */
+char *
+ops_ether_ntoa(char *mac_a, const struct ether_addr *addr)
+{
+	(void)snprintf(mac_a, OPS_MAC_STR_SIZE, "%02x:%02x:%02x:%02x:%02x:%02x",
+			addr->ether_addr_octet[0], addr->ether_addr_octet[1],
+			addr->ether_addr_octet[2], addr->ether_addr_octet[3],
+			addr->ether_addr_octet[4], addr->ether_addr_octet[5]);
+	return mac_a;
+} /* ops_ether_ntoa */
+
+/*
+ * ops_wwn_ntoa - World Wide Name version of ops_ether_ntoa
+ *
+ * converts a WWN address pointed to by wwn to a string wwn_a
+ * with leading zeros. returns wwn_a
+ */
+char *
+ops_wwn_ntoa(char *wwn_a, const char *wwn)
+{
+	(void)snprintf(wwn_a, OPS_WWN_STR_SIZE, "%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x",
+			wwn[0], wwn[1], wwn[2], wwn[3], wwn[4], wwn[5], wwn[6], wwn[7]);
+	return wwn_a;
+} /* ops_wwn_ntoa */
+
+/*
+ * ops_ether_array_to_string
+ *
+ * converts an Ethernet address stored as a 6 byte binary array
+ * into a printable mac address with leading zeros. returns mac_a.
+ *
+ */
+char *
+ops_ether_array_to_string(char *mac_a, const unsigned char *addr)
+{
+	(void)snprintf(mac_a, OPS_MAC_STR_SIZE, "%02x:%02x:%02x:%02x:%02x:%02x",
+			addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+	return mac_a;
+} /* ops_ether_array_to_string */
+
+/*
+ * ops_ether_ulong_long_to_string
+ *
+ * converts an Ethernet address stored as a long long
+ * into a printable mac address with leading zeros. returns mac_a.
+ *
+ */
+char *
+ops_ether_ulong_long_to_string(char *mac_a, const unsigned long long mac)
+{
+	unsigned char addr[6];
+	unsigned long long max_mac = 0xffffffffffff;
+
+	if (mac > max_mac) {
+		return ( (char *) NULL);
+	}
+	ops_ulong_long_to_char_array(mac, ETH_ALEN, addr);
+
+	(void)snprintf(mac_a, OPS_MAC_STR_SIZE,
+		       "%02x:%02x:%02x:%02x:%02x:%02x",
+		       addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+
+	return mac_a;
+} /* ops_ether_ulong_long_to_string */
